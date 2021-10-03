5957fa0b374fac5a06670ccb044c9ef02f80458e
Emil Gusgård <gusemil@hotmail.com>
2021-10-03 21:06:07 +0300

    final clean up

--- apicontroller.js
+++ apicontroller.js
@@ -23,58 +23,12 @@ module.exports = function(app, mysql){
             function(err, rows){
                 
                 if(err) throw err;
-                console.log("ROW 0");
-                console.log(rows[0].id);
-                console.log(rows[0].name);
-                console.log(rows[0].score);
-                console.log(rows[0].waves);
-                console.log(rows[0].faction);
-                console.log("ROW 1");
-                console.log(rows[1].id);
-                console.log(rows[1].name);
-                console.log(rows[1].score);
-                console.log(rows[1].waves);
-                console.log(rows[1].faction);
-
-                console.log("ROWS");
                 console.log(rows);
                 var JsonString = JSON.stringify(rows);
                 console.log(JsonString);
 
                 //returned as JSON (to Unity)
                 res.json({rows});
-            }
-        );
-        // Close database connection
-        con.end();
-    });
-
-    
-    app.get('/rts/rts/:id', function(req,res){
-        console.log("Run a get request from Unity!");
-
-        // Create connection with parameters
-        var con = mysql.createConnection({
-
-            host: "localhost",
-            user: "root",
-            password: "",
-            database: "rts"
-        });
-        con.connect();
-        
-        // Query by id
-        con.query('SELECT id, name, score, waves, faction FROM rts WHERE id='+req.params.id, 
-            function(err, rows){
-                
-                if(err) throw err;
-                console.log(rows[0].id);
-                console.log(rows[0].name);
-                console.log(rows[0].score);
-                console.log(rows[0].waves);
-                console.log(rows[0].faction);
-                //returned as JSON (to Unity)
-                res.json({id: rows[0].id, name: rows[0].name, score:rows[0].score, waves:rows[0].waves, faction:rows[0].faction});
             }
         );
         // Close database connection
@@ -111,11 +65,4 @@ module.exports = function(app, mysql){
         res.json({name: req.body.name, score: req.body.score, waves: req.body.waves, faction: req.body.faction});
 
     });
-
-    /*
-    app.delete('/rts/rts/:id', function(req,res){
-        //delete later?
-
-    });
-    */
 }
